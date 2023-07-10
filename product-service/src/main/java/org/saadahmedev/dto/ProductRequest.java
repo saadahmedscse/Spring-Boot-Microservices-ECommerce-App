352224f3690ee67e14c57335e413d6e9debcd42b
saadahmedscse <saadahmedscse@gmail.com>
2023-07-10 23:13:26 +0600

    added product request model class

---
+++ product-service/src/main/java/org/saadahmedev/dto/ProductRequest.java
@@ -0,0 +1,18 @@
+package org.saadahmedev.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
