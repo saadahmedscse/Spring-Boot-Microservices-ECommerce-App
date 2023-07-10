28039987ee4015ffb77a400d22b47108d19d08ac
saadahmedscse <saadahmedscse@gmail.com>
2023-07-10 23:23:56 +0600

    created get all products endpoint

--- product-service/src/main/java/org/saadahmedev/controller/ProductController.java
+++ product-service/src/main/java/org/saadahmedev/controller/ProductController.java
@@ -1,11 +1,14 @@
 package org.saadahmedev.controller;
 
 import org.saadahmedev.dto.ProductRequest;
+import org.saadahmedev.dto.ProductResponse;
 import org.saadahmedev.model.ApiResponseBody;
 import org.saadahmedev.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -19,5 +22,11 @@ public class ProductController {
     public ApiResponseBody createProduct(ProductRequest productRequest) {
         productService.createProduct(productRequest);
         return new ApiResponseBody(true, "Product created successfully");
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getProducts() {
+        return productService.getProducts();
     }
 }
