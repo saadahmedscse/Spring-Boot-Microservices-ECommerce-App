ab4c071a92aa53c9f8cf763104f2582902831ecc
saadahmedscse <saadahmedscse@gmail.com>
2023-07-10 23:24:17 +0600

    created method for getting all products

--- product-service/src/main/java/org/saadahmedev/service/ProductService.java
+++ product-service/src/main/java/org/saadahmedev/service/ProductService.java
@@ -2,10 +2,13 @@ package org.saadahmedev.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.saadahmedev.dto.ProductRequest;
+import org.saadahmedev.dto.ProductResponse;
 import org.saadahmedev.model.Product;
 import org.saadahmedev.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -24,5 +27,19 @@ public class ProductService {
         productRepository.save(product);
 
         log.info("Product {} saved successfully", product.getId());
+    }
+
+    public List<ProductResponse> getProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice()).build();
     }
 }
