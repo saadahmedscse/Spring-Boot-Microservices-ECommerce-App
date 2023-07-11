2f3b661da4a3241c37ff305107e125b5596dd2d1
saadahmedscse <saadahmedscse@gmail.com>
2023-07-11 16:23:14 +0600

    order line item entity class

---
+++ order-service/src/main/java/org/saadahmedev/model/OrderLineItem.java
@@ -0,0 +1,24 @@
+package org.saadahmedev.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "t_order_line_item")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderLineItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
