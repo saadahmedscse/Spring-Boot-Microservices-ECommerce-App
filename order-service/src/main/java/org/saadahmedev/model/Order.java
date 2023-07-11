6bc281abed30599f0350b6b692d2d11f05473ed8
saadahmedscse <saadahmedscse@gmail.com>
2023-07-11 16:23:03 +0600

    order entity class

---
+++ order-service/src/main/java/org/saadahmedev/model/Order.java
@@ -0,0 +1,24 @@
+package org.saadahmedev.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "t_order")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItem> orderLineItems;
+}
