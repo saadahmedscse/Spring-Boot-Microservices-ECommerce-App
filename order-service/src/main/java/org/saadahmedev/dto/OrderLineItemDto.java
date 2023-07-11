d03d5f63ccc3a03006999352b2674000c4559e0a
saadahmedscse <saadahmedscse@gmail.com>
2023-07-11 16:24:06 +0600

    created dto of order line item

---
+++ order-service/src/main/java/org/saadahmedev/dto/OrderLineItemDto.java
@@ -0,0 +1,19 @@
+package org.saadahmedev.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderLineItemDto {
+    private long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
