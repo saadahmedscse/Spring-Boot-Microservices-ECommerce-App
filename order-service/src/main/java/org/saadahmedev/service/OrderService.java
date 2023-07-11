66f07d1db4d8c0d418add724b186490da6b0de38
saadahmedscse <saadahmedscse@gmail.com>
2023-07-11 16:24:51 +0600

    created order service for placing order

---
+++ order-service/src/main/java/org/saadahmedev/service/OrderService.java
@@ -0,0 +1,34 @@
+package org.saadahmedev.service;
+
+import org.saadahmedev.dto.OrderLineItemDto;
+import org.saadahmedev.dto.OrderRequest;
+import org.saadahmedev.model.Order;
+import org.saadahmedev.model.OrderLineItem;
+import org.saadahmedev.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = Order.builder()
+                .orderNumber(UUID.randomUUID().toString())
+                .orderLineItems(orderRequest.getOrderLineItemDtos()
+                        .stream().map(this::mapToOrderLineItem).toList()).build();
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItem mapToOrderLineItem(OrderLineItemDto orderLineItemDto) {
+        return OrderLineItem.builder()
+                .skuCode(orderLineItemDto.getSkuCode())
+                .price(orderLineItemDto.getPrice())
+                .quantity(orderLineItemDto.getQuantity()).build();
+    }
+}
