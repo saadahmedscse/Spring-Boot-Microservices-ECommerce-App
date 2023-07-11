2aa4c469217baa64fcdf7192114d1d9199b3de61
saadahmedscse <saadahmedscse@gmail.com>
2023-07-11 17:38:16 +0600

    added method to find by skuCode list

---
+++ inventory-service/src/main/java/org/saadahmedev/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package org.saadahmedev.repository;
+
+import org.saadahmedev.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
