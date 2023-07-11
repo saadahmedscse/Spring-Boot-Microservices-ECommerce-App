53c6b953da54d6803552c2a6d52e38a2862c3e13
saadahmedscse <saadahmedscse@gmail.com>
2023-07-11 17:39:01 +0600

    created inventory service to check is in stock or not

---
+++ inventory-service/src/main/java/org/saadahmedev/service/InventoryService.java
@@ -0,0 +1,29 @@
+package org.saadahmedev.service;
+
+import org.saadahmedev.dto.InventoryResponse;
+import org.saadahmedev.model.Inventory;
+import org.saadahmedev.repository.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class InventoryService {
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(this::mapToInventoryResponse).toList();
+    }
+
+    private InventoryResponse mapToInventoryResponse(Inventory inventory) {
+        return InventoryResponse.builder()
+                .skuCode(inventory.getSkuCode())
+                .isInStock(inventory.getQuantity() > 0).build();
+    }
+}
