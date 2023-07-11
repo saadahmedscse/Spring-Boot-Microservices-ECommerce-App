164a3fa37628e60513d3e3b026750d33e80ef199
saadahmedscse <saadahmedscse@gmail.com>
2023-07-11 16:23:47 +0600

    added endpoint for placing order

---
+++ order-service/src/main/java/org/saadahmedev/controller/OrderController.java
@@ -0,0 +1,23 @@
+package org.saadahmedev.controller;
+
+import org.saadahmedev.dto.ApiResponseBody;
+import org.saadahmedev.dto.OrderRequest;
+import org.saadahmedev.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponseBody placeOrder(OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return new ApiResponseBody(true, "Order created successfully");
+    }
+}
